ad1a43588326ec078d35330bde63f75ae2a03265
Tarakanov <anniktar11@gmail.com>
2017-06-30 14:29:31 +0500

    logic

--- script.js
+++ script.js
@@ -1,25 +1,52 @@
 "use strict";
 
-let inputBlock = document.getElementById("options");
-inputBlock.addEventListener("click", acceptData);
+let doCredit = document.getElementById("do-credit");
+doCredit.addEventListener("click", getCredit);
+let doEarly = document.getElementById("do-early");
+doEarly.addEventListener("click", getEarly);
 
-function acceptData (){
-    let dataCredit = {
-        number: document.getElementById("sum").value,
-        countMonths: document.getElementById("count").value,
-        percent: document.getElementById("percent").value,
-        outputText: document.getElementById("outputText")
-    };
-    console.log("dataCredit.percent-dataCredit.countMonths --- "+dataCredit.percent+"-"+dataCredit.countMonths);
-    dataCredit.percentMonths = dataCredit.percent/12/100;
-
-    outputText.textContent = calculate(dataCredit);
+function getCredit (){
+    let dataCredit = getData("credit");
+    outPay(calculateCredit(dataCredit), "credit");
 }
 
-function calculate(data){
+function getEarly () {
+    let dataEarly = getData("early");
+    let amount = document.getElementById("amount-early").value;
+    let payOne = calculateCredit(dataEarly);
+    dataEarly.number = dataEarly.number - amount;
+    let payTwo = calculateCredit(dataEarly);
+    let economy = getEconomy(payOne, payTwo, dataEarly.countMonths)-amount;
+    outEconomy(payTwo, economy);
+}
+
+function outEconomy (pay, economy) {
+    let outPay = document.getElementById('out-early');
+    outPay.textContent = pay;
+    let outSaving = document.getElementById('saving');
+    outSaving.textContent = economy;
+}
+
+function getData (id) {
+    let data = {
+        number: document.getElementById("sum-"+id).value,
+        countMonths: document.getElementById("count-"+id).value,
+        percent: document.getElementById("percent-"+id).value,
+    };
+    return data;
+}
+
+function getEconomy (x, y, count) {
+    return x*count-y*count;
+}
+
+function calculateCredit (data){
+    data.percentMonths = data.percent/12/100;
     let tempValue = Math.pow((1+data.percentMonths),data.countMonths);
-    console.log("percentMonths = "+data.percentMonths+".");
-    console.log("tempValue = "+tempValue+".");
     return (data.number*data.percentMonths*tempValue)/(tempValue-1);
 }
 
+function outPay (pay, id){
+    let out = document.getElementById("out-"+id);
+    out.textContent = pay;
+}
